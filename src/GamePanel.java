75e21a9218bb5cff5d726f9a897b48c480acf98c
Luis Combariza <luis_combariza@outlook.com>
2020-11-29 12:25:23 -0800

    Apple method to draw apple randomly on gamepannel complete

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -24,7 +24,7 @@ public class GamePanel extends JPanel implements ActionListener {
     Timer timer;
     Random random;
 
-    
+
     GamePanel(){
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
@@ -42,15 +42,26 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
 
     }
     public void draw(Graphics g){
 
+        for(int i=0; i<SCREEN_HEIGHT/UNIT_SIZE; i++){
+            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX,appleY, UNIT_SIZE, UNIT_SIZE);
     }
     public void newApple(){
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
 
     }
     public void move(){
+        for
 
     }
 
